a1363878e628d9a8beff9dda7998cb10273244e0
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-09 10:16:19 +0100

    created properties and constructor for passenger

--- src/main/java/com/example/Airline/models/Passenger.java
+++ src/main/java/com/example/Airline/models/Passenger.java
@@ -1,4 +1,16 @@
 package com.example.Airline.models;
 
 public class Passenger {
+
+//    POJO
+
+    private int id;
+    private String name;
+    private String emailAddress;
+
+    public Passenger(int id, String name, String emailAddress) {
+        this.id = id;
+        this.name = name;
+        this.emailAddress = emailAddress;
+    }
 }
