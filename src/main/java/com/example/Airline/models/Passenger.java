711ac0d72d34ad37f6ead68439b68c7a63941e6c
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-09 10:17:48 +0100

    getters and setters for passenger

--- src/main/java/com/example/Airline/models/Passenger.java
+++ src/main/java/com/example/Airline/models/Passenger.java
@@ -4,13 +4,48 @@ public class Passenger {
 
 //    POJO
 
+    //properties
     private int id;
     private String name;
     private String emailAddress;
 
+    //constructor
     public Passenger(int id, String name, String emailAddress) {
         this.id = id;
         this.name = name;
+        this.emailAddress = emailAddress;
+    }
+
+    //empty contructor
+
+    public Passenger(){
+
+    };
+
+    //getters and setters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
 }
