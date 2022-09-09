a6e117332128a34192f90d1957fcaab7e3258540
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-09 10:36:24 +0100

    added services

--- src/main/java/com/example/Airline/controllers/FlightsController.java
+++ src/main/java/com/example/Airline/controllers/FlightsController.java
@@ -1,6 +1,7 @@
 package com.example.Airline.controllers;
 
 import com.example.Airline.models.Flight;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/com/example/Airline/services/FlightService.java
@@ -0,0 +1,4 @@
+package com.example.Airline.services;
+
+public class FlightService {
+}
