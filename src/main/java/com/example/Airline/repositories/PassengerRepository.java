edf6c97a240ff9b4d9a61b36babe561ea8f3a321
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-09 10:37:59 +0100

    added repositories

---
+++ src/main/java/com/example/Airline/repositories/PassengerRepository.java
@@ -0,0 +1,4 @@
+package com.example.Airline.repositories;
+
+public interface PassengerRepository {
+}
--- src/main/java/com/example/Airline/services/FlightService.java
+++ src/main/java/com/example/Airline/services/FlightService.java
@@ -1,4 +1,9 @@
 package com.example.Airline.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class FlightService {
+
+
 }
