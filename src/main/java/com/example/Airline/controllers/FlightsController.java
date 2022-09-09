de6b74153c29a1172c62080f37829513ebaaad4d
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-09 10:34:00 +0100

    controllers

--- src/main/java/com/example/Airline/controllers/FlightsController.java
+++ src/main/java/com/example/Airline/controllers/FlightsController.java
@@ -1,4 +1,23 @@
 package com.example.Airline.controllers;
 
+import com.example.Airline.models.Flight;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(value = "/flights")
 public class FlightsController {
+
+//    INDEX
+   @GetMapping
+    public ResponseEntity<List<Flight>> getAllFlights(){
+       List<Flight> flights = flightService.getAllFlights();
+       return new ResponseEntity<>(flights, HttpStatus.OK);
+   }
+
 }
